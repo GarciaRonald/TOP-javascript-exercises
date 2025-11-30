6758607845d20dace0851825d6c656b6e3090048
Ronald Garcia <ronaldgm@gmail.com>
2025-11-29 19:17:54 -0500

    Fix function and pass test in 08...

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, arg) {
+    let myArgs = Object.values(arguments);
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 1; j < myArgs.length; j++) {
+            if (arr.includes(myArgs[j])) {
+                arr.splice(arr.indexOf(myArgs[j]), 1);
+            }
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
