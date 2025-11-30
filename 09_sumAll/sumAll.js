627092478148b4cd42c0856ffd1921055d8bcc8c
Ronald Garcia <ronaldgm@gmail.com>
2025-11-29 19:45:42 -0500

    Fix function and pass test in 09...

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return 'ERROR';
+    } else if (a < 0 || b < 0) {
+        return 'ERROR';
+    } else if (a % 1 > 0 || b % 1 > 0) {
+        return 'ERROR';
+    }
 
+    let range = [a, b].sort((a, b) => a - b);
+    let sum = 0;
+
+    for (let i = range[0]; i <= range[1]; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
